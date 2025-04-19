b87d25ce5b2f571b5ee69b8bb4d6d3ebb2d0e77d
Captain Coder <captaincodersacademy@gmail.com>
2025-04-19 10:22:50 -0700

    jamming: First draft of scrap UI

--- Unity Project/DCJam2025/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/CrawlingMode/PartyData.cs
+++ Unity Project/DCJam2025/Assets/_Libs/CaptainCoder.Dungeoneering.Encounter/src/CrawlingMode/PartyData.cs
@@ -8,6 +8,17 @@ namespace CaptainCoder.Dungeoneering.Unity
     [CreateAssetMenu(menuName = "DC/PartyData")]
     public class PartyData : ObservableSO
     {
+        [SerializeField] private int _gold;
+        public int Gold
+        {
+            get => _gold;
+            set
+            {
+                _gold = value;
+                OnGoldChanged?.Invoke(_gold);
+            }
+        }
+        public event System.Action<int> OnGoldChanged;
         [field: Expandable][field: SerializeField] public HeroEntityData[] Heroes { get; private set; }
         [field: Expandable][field: SerializeField] public ContainerData PartyInventory { get; private set; }
         [field: Expandable][field: SerializeField] public PlayerViewData PlayerView { get; private set; }
@@ -20,6 +31,17 @@ namespace CaptainCoder.Dungeoneering.Unity
                 hero.Wounds = 0;
                 hero.Exertion = 0;
             }
+        }
+
+        protected override void OnExitPlayMode()
+        {
+            base.OnExitPlayMode();
+            OnGoldChanged = null;
+        }
+
+        void OnValidate()
+        {
+            OnGoldChanged?.Invoke(_gold);
         }
 
         [Button]
